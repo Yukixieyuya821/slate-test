8102bc3b54ef7a4bea7539a836e560d656a877fc
yuki.xie <yu.xie@wiz.ai>
2022-09-16 17:25:44 +0800

    eslint

---
+++ vite-react-ts-eslint/src/App.tsx
@@ -0,0 +1,9 @@
+import RichTextExample from './components/richtext';
+const App = () => (
+    <div className="App">
+        <h1 className="title">RichTextExample</h1>
+        <RichTextExample />
+    </div>
+);
+
+export default App;
